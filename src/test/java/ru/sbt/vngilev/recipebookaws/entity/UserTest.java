3a3899103eaccb66fb84e53886aa7ea2fce5c0d6
vngilev <vngilev@yandex.ru>
2018-10-09 18:11:29 +0700

    Изменена конфигурация проекта

--- src/test/java/ru/sbt/vngilev/recipebookaws/entity/UserTest.java
+++ src/test/java/ru/sbt/vngilev/recipebookaws/entity/UserTest.java
@@ -6,13 +6,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:"})
+@ContextConfiguration(locations = {"file:../../../../../../../main/resources/recipebook-application.xml"})
 public class UserTest {
-
 
     @Before
     public void setUp() {
 
     }
+
 }
 
